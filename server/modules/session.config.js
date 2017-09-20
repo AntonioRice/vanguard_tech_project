a10a6b3ef4853408f5631a32f226a1554e52b328
Antonio Rice <ricex407@gmail.com>
2017-09-19 22:45:37 -0500

    updated auth

--- server/modules/session.config.js
+++ server/modules/session.config.js
@@ -5,5 +5,6 @@ module.exports = session({
    key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
    resave: 'true',
    saveUninitialized: false,
-   cookie: { maxage: 60000, secure: false }
+   cookie: { maxage: 60000, secure: false}
+  //  cookie: { maxage: 60000, secure: true, httpOnly: true }
 });
